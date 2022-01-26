dfa4768eed007d145ea02e4a19ded02036813230
DBailie <DanielB@nebula.co.za>
2022-01-26 13:37:13 +0200

    Daniel's Dojo

---
+++ CrisisCommunication/Server/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CrisisCommunication.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrisisCommunication.Server.Data
+{
+    public class AppDbContext: DbContext
+    {
+        private AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        DbSet<User> Users { get; set; }
+    }
+}
